88d259a1f0535b9c0163a1e49963571e040ab9ac
Andrea Parodi <andrea@parro.it>
2015-11-24 22:51:28 +0100

    sublime pkg & prefs

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 ln -sf $(pwd)/.zshrc ~/.zshrc
+ln -sf $(pwd)/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sf $(pwd)/Installed\ Packages/Package\ Control.sublime-package ~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package
+
+mkdir ~/.fonts && cp ~/Dropbox/Input_Fonts/Input/* ~/.fonts
